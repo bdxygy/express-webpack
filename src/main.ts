61b85559067e92843a2ff770be711a6b1777b17d
budi santoso <budi.santoso@salt.co.id>
2023-06-03 15:14:47 +0700

    delete unused in base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import express, { type Application } from "express";
 import { config } from "dotenv";
 import path from "path";
-import adminRouter from "@routes/admin";
-import frontRouter from "@routes/front";
+import baseRouter from "@routes/base";
 
 config({
   path: path.resolve(__dirname, `.env.${process.env.STAGE}`),
@@ -10,13 +9,15 @@ config({
 
 const app: Application = express();
 
-app.use("/admin", adminRouter);
-app.use("/", frontRouter);
+app.use("/", baseRouter);
+
 app.use((req, res) => {
   res.write("Page Not Found Again!");
   res.end();
 });
 
 app.listen(5000, () => {
-  console.log(`Your server is Running in ${process.env.STAGING} Mode! Please Aware Again`);
+  console.log(
+    `Your server is Running in ${process.env.STAGING} Mode! Please Aware Again`
+  );
 });
